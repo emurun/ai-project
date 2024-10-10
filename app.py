3b84c10b8ec325d549be58151f23fe98a31f8a64
Murun Enkhtaivan <murunenkhtaivan@Airbook.local>
2024-10-09 19:48:35 -0700

    origin update

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ port = 10000
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://personal-web-y2l6.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
